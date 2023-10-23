1575832ba128451394c31055ea867687a9500849
rattus-aristarchus <lankin.m.a@gmail.com>
2023-10-24 01:07:35 +0300

    parameterized fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,19 @@ from selene import browser
 from selenium import webdriver
 
 
+DESKTOP_RESOLUTIONS = [
+    [1920, 1080],
+    [1600, 1200]
+]
+
+MOBILE_RESOLUTIONS = [
+    [360, 800],
+    [412, 915]
+]
+
+RESOLUTIONS = DESKTOP_RESOLUTIONS + MOBILE_RESOLUTIONS
+
+
 @pytest.fixture(scope="function")
 def browser_desktop():
     browser.config.driver = webdriver.Chrome()
@@ -17,8 +30,19 @@ def browser_desktop():
 @pytest.fixture(scope="function")
 def browser_mobile():
     browser.config.driver = webdriver.Chrome()
-    browser.config.window_width = 320
-    browser.config.window_height = 593
+    browser.config.window_width = 360
+    browser.config.window_height = 800
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(params=RESOLUTIONS)
+def browser_setup(request):
+    browser.config.driver = webdriver.Chrome()
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
 
     yield
 
--- tests/test_skip.py
+++ tests/test_skip.py
@@ -3,16 +3,34 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene import browser
+
+from tests.models.github_home_page import GitHubHomePage
+from tests.conftest import MOBILE_RESOLUTIONS, DESKTOP_RESOLUTIONS
 
 
-@pytest.fixture()
-def browser():
-    pass
+def resolution():
+    return [browser.config.window_width,
+            browser.config.window_height]
 
 
-def test_github_desktop(browser):
-    pass
+def test_github_desktop(browser_setup):
+    if resolution() not in DESKTOP_RESOLUTIONS:
+        pytest.skip()
+
+    page = GitHubHomePage()
+    page.open()
+    page.sign_in_desktop()
+
+    page.should_show_auth_form()
 
 
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(browser_setup):
+    if resolution() not in MOBILE_RESOLUTIONS:
+        pytest.skip()
+
+    page = GitHubHomePage()
+    page.open()
+    page.sign_in_mobile()
+
+    page.should_show_auth_form()
